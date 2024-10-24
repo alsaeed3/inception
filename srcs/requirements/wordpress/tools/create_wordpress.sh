674de5c4fa5b758af624c744adca39b6ea81e00c
alsaeed3 <ali.eissa.570@gmail.com>
2024-10-24 19:22:22 +0400

    fixed the wp-config.php & the url for the websites thro editing /etc/hosts

--- srcs/requirements/wordpress/tools/create_wordpress.sh
+++ srcs/requirements/wordpress/tools/create_wordpress.sh
@@ -1,40 +1,33 @@
 #!/bin/sh
 
+wp core download --allow-root --locale=en_US
+
 # Check if wp-config.php exists
-if [ -f ./wp-config.php ]; then
-    echo "WordPress already downloaded"
-else
+if [ ! -f "/var/www/wp-config.php" ]; then
+
+	wp config create --allow-root --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb --dbcharset="utf8" 
+fi
 
 ####### MANDATORY PART ##########
 
-    # Download WordPress and all config files
-    wget http://wordpress.org/latest.tar.gz
-    tar xfz latest.tar.gz
-    mv wordpress/* .
-    rm -rf latest.tar.gz
-    rm -rf wordpress
+# Install WordPress
+wp core install --url="$DOMAIN_NAME" \
+	--title="Ali Saeed's WordPress" \
+	--admin_user="$MYSQL_USER" \
+	--admin_password="$MYSQL_PASSWORD" \
+	--admin_email="$WP_ADMIN_EMAIL" \
+	--path=/var/www/html/ \
+	--allow-root
 
-    # Import env variables in the config file
-    sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
-    sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
-    sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
-    sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
-    cp wp-config-sample.php wp-config.php
+wp user create --allow-root ${WP_USER_LOGIN} ${WP_USER_EMAIL} \
+--role=author \
+--user_pass=${WP_USER_PASSWORD} \
 
-    # Fetch and update the authentication keys and salts
-    new_keys=$(curl -s https://api.wordpress.org/secret-key/1.1/salt/)
-    sed -i "/AUTH_KEY/c\\$new_keys" wp-config.php
+wp option update blogname "Ali Saeed's WordPress" --allow-root
+wp option update blogdescription "Inception" --allow-root
+wp option update blog_public 0 --allow-root
 
-    # Install WordPress
-    wp core install --url="https://${DOMAIN_NAME}/" --title="Ali Saeed" --admin_user="${MYSQL_USER}" --admin_password="${MYSQL_PASSWORD}" --admin_email="${WP_EMAIL}" --path=/var/www/html/ --allow-root
-    wp user create --allow-root ${WP_USER_LOGIN} ${WP_USER_EMAIL} --user_pass=${WP_USER_PASSWORD}
-    if [ $? -eq 0 ]; then
-        echo "WordPress installed successfully."
-    else
-        echo "Error installing WordPress."
-        exit 1
-    fi
-fi
+wp plugin update --all --allow-root
 
 chown -R www-data:www-data /var/www/html/
 chmod -R 755 /var/www/html/
